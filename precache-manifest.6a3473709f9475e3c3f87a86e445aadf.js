e4aed4290d6cb2e193833c1ac5cf35e962265422
Taniyah-L-Jackson <taniyah-l-jackson-h@gmail.com>
2020-06-18 13:10:18 -0500

    Updates

---
+++ precache-manifest.6a3473709f9475e3c3f87a86e445aadf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80710defc0ed2028a95d017bca733920",
+    "url": "/ocean_todolist/index.html"
+  },
+  {
+    "revision": "0787ab2895f5b52f828d",
+    "url": "/ocean_todolist/static/js/2.4841ec6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ocean_todolist/static/js/2.4841ec6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da7431c247c0ab479d2",
+    "url": "/ocean_todolist/static/js/main.7799c2fd.chunk.js"
+  },
+  {
+    "revision": "39fdd685a83536ecbdb7",
+    "url": "/ocean_todolist/static/js/runtime-main.928735ca.js"
+  }
+]);
